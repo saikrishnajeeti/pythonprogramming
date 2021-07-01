7e8ced569db1c13267978c64b5ed8d7098685f01
saikrishnajeeti <86720851+saikrishnajeeti@users.noreply.github.com>
2021-07-01 18:07:33 +0530

    Add files via upload

---
+++ ex-3.py
@@ -0,0 +1,21 @@
+'''
+you can assign the values to a variable in 2 types
+1)a,b,c,d,e=10,20,30,40,50
+2)a=10
+b=20
+c=30
+d=40
+e=50
+'''
+a=10
+b=4.0
+c=30
+d=90
+e=a-b
+f=e*d+c-a
+print("a=",a)
+print("b=",b)
+print("c=",c)
+print("d=",d)
+print("e=",e)
+print("f=",f)
