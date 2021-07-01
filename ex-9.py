9216c053d01ccd5c0e9316ee472c895b511c87c4
saikrishnajeeti <86720851+saikrishnajeeti@users.noreply.github.com>
2021-07-01 21:49:20 +0530

    functions and return

---
+++ ex-9.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    c=a+b
+    return c
+def sub(e,f):
+    d=e-f
+    return d
+def mul(g,h):
+    i=g*h
+    return i
+
+n1=add(10,20)
+n2=sub(50,40)
+n3=mul(10,30)
+print(n1)
+print(n2)
+print(n3)
